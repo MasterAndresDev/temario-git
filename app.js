60b747bfba3ab2bef41fd4dc6c0065f5856764e9
MasterAndresDev <andresdevmaster@gmail.com>
2026-09-22 12:29:51 -0600

    Agregar version de aplicacion

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 console.log("Hello, World!");
 console.log("This is a sample application.");
 
+const version = "1.0.0";
+console.log("Application version: " + version);
+
 function saludar(nombre) {
     console.log("hola " + nombre);
 }
