1f35c8274f2069e9aa48f741d050e8925abfdb75
MasterAndresDev <andresdevmaster@gmail.com>
2026-09-22 13:34:49 -0600

    guardar

--- app.js
+++ app.js
@@ -16,18 +16,15 @@ function despedir(nombre) {
 
 despedir("Juan");
 
-<<<<<<< HEAD
 function restar(a, b) {
     return a - b;
 }
 
-const resultado = restar(10, 5);
-console.log("Resultado de la resta: " + resultado);
-=======
+const resultado1 = restar(10, 5);
+console.log("Resultado de la resta: " + resultado1);
 function sumar(a, b){
     return a + b;
 }
 
-const resultado = sumar(5, 3);
-console.log("El resultado de la suma es: " + resultado);
->>>>>>> feature-despedida
+const resultado2 = sumar(5, 3);
+console.log("El resultado de la suma es: " + resultado2);
